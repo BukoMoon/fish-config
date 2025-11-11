d85a052d73e46c655fc29d143b10fef90eefe98d
BukoMoon <louisadams228@gmail.com>
2025-11-12 08:04:16 +1100

    Initial commit

---
+++ functions/ytmusicdl.fish
@@ -0,0 +1,3 @@
+function ytmusicdl --wraps='~/.config/fish/scripts/yt-music-album-download.sh' --description 'alias ytmusicdl=~/.config/fish/scripts/yt-music-album-download.sh'
+    ~/.config/fish/scripts/yt-music-album-download.sh $argv
+end
